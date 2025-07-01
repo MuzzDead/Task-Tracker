00b11c097854cc31d1dd661515affe4f771f639f
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-01 17:15:59 +0300

    add-enums

---
+++ TaskTracker.Domain/Enums/Visibility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Domain.Enums;
+
+public enum Visibility
+{
+    Private = 0,
+    Public = 1,
+    Team = 2
+}
