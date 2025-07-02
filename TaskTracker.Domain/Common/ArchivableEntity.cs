78f709df29d88d7818330d44f0059980171aacad
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-02 12:59:15 +0300

    updated-common

--- TaskTracker.Domain/Common/ArchivableEntity.cs
+++ TaskTracker.Domain/Common/ArchivableEntity.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TaskTracker.Domain.Common;
 
-public abstract class BaseEntity
+public abstract class ArchivableEntity
 {
-    public Guid Id { get; protected set; } = Guid.NewGuid();
+    public bool IsArchived { get; set; }
+    public DateTimeOffset ArchivedAt { get; set; }
 }
---
+++ TaskTracker.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Domain.Common;
+
+public abstract class BaseAuditableEntity : BaseEntity
+{
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public string CreatedBy { get; set; } = string.Empty;
+
+    public DateTimeOffset UpdatedAt { get; set; }
+    public string? UpdatedBy { get; set;}
+}
