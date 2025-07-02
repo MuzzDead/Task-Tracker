fecdca9bdbd82e4ed7627e4a7ec4541b787dc89e
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-02 18:40:32 +0300

    added-mapping

---
+++ TaskTracker.Application/Mappings/BoardRoleProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.DTOs;
+using TaskTracker.Domain.Entities;
+
+namespace TaskTracker.Application.Mappings;
+
+public class BoardRoleProfile : Profile
+{
+    public BoardRoleProfile()
+    {
+        CreateMap<BoardRole, BoardRoleDto>().ReverseMap();
+    }
+}
---
+++ TaskTracker.Application/Mappings/CommentProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.DTOs;
+using TaskTracker.Domain.Entities;
+
+namespace TaskTracker.Application.Mappings;
+
+public class CommentProfile : Profile
+{
+    public CommentProfile()
+    {
+        CreateMap<Comment, CommentDto>().ReverseMap();
+    }
+}
