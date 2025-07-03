099c501802fec3edb7b7bfe1dd14e6c8f41da837
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-04 00:49:23 +0300

    add-service-collection-extensions

---
+++ TaskTracker.Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces;
+using TaskTracker.Application.Common.Interfaces.Base;
+using TaskTracker.Persistence.Repositories;
+using TaskTracker.Persistence.Repositories.Base;
+
+namespace TaskTracker.Persistence.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddPersistenceServices(
+        this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped(typeof(IRepository<,>), typeof(BaseRepository<,>));
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IBoardRoleRepository, BoardRoleRepository>();
+        services.AddScoped<IBoardRepository, BoardRepository>();
+        services.AddScoped<ICardRepository, CardRepository>();
+        services.AddScoped<IColumnRepository, ColumnRepository>();
+        services.AddScoped<IStateRepository, StateRepository>();
+        services.AddScoped<ICommentRepository, CommentRepository>();
+
+        return services;
+    }
+}
