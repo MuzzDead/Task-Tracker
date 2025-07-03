dc919d031946d02b9479c98bc57789bd5e462599
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-03 22:34:28 +0300

    added-comment-repository

---
+++ TaskTracker.Persistence/Repositories/CommentRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces;
+using TaskTracker.Domain.Entities;
+using TaskTracker.Persistence.Repositories.Base;
+
+namespace TaskTracker.Persistence.Repositories;
+
+public class CommentRepository : BaseRepository<Comment, Guid>, ICommentRepository
+{
+    public CommentRepository(ApplicationDbContext context) : base(context) { }
+
+    public async Task<IEnumerable<Comment>> GetByCardId(Guid cardId)
+    {
+        var comments = await _dbSet
+            .Where(c => c.CardId == cardId)
+            .OrderBy(c => c.CreatedAt)
+            .ToListAsync();
+
+        return comments;
+    }
+}
