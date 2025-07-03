63f5edbdf19791339cf9dc08ad60051b9ea14c6e
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-03 11:16:37 +0300

    add-Interface-methods-for-board

--- TaskTracker.Application/Common/Interfaces/IBoardRepository.cs
+++ TaskTracker.Application/Common/Interfaces/IBoardRepository.cs
@@ -5,9 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using TaskTracker.Application.Common.Interfaces.Base;
 using TaskTracker.Domain.Entities;
+using TaskTracker.Domain.Enums;
 
 namespace TaskTracker.Application.Common.Interfaces;
 
 public interface IBoardRepository : IRepository<Board>
 {
+    Task CreateBoardAsync(Board board, Guid userId, UserRole userRole);
+    Task RemoveUserFromBoardAsync(Guid boardId,Guid userId);
+    Task ArchiveAsync(Guid boardId);
 }
