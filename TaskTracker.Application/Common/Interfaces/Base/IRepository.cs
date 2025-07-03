10a0128185b733abb0886d34b5199e1b27f289c7
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-03 10:17:20 +0300

    added-interface-for-repositories

---
+++ TaskTracker.Application/Common/Interfaces/Base/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Application.Common.Interfaces.Base;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(Guid Id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(Guid Id);
+}
