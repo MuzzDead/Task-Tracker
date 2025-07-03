569566df4155bacb23d388d120761b70bf268480
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-03 21:47:35 +0300

    upd-base-entity

--- TaskTracker.Domain/Common/ArchivableEntity.cs
+++ TaskTracker.Domain/Common/ArchivableEntity.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace TaskTracker.Domain.Common;
 
-public abstract class ArchivableEntity
+public abstract class ArchivableEntity : BaseAuditableEntity
 {
     public bool IsArchived { get; set; }
     public DateTimeOffset ArchivedAt { get; set; }
