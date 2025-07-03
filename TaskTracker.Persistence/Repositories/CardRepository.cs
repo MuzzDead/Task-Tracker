2cac362cade5c479d8b37e1aecb5f138076fd7e5
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-03 21:53:18 +0300

    added-card-repository

---
+++ TaskTracker.Persistence/Repositories/CardRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces;
+using TaskTracker.Persistence.Repositories.Base;
+using TaskTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskTracker.Persistence.Repositories;
+
+public class CardRepository : BaseRepository<Card, Guid>, ICardRepository
+{
+    public CardRepository(ApplicationDbContext context) : base(context) { }
+
+    public async Task<IEnumerable<Card>> GetByColumnIdAsync(Guid columnId)
+    {
+        return await _dbSet
+            .Where(c => c.ColumnId == columnId)
+            .OrderBy(c => c.RowIndex)
+            .ToListAsync();
+    }
+}
