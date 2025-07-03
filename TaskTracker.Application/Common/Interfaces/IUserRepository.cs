a8aec68e1e96433d5d8a8588ecb3bbd546b55da4
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-03 11:36:25 +0300

    add-Interface-methods-for-user

--- TaskTracker.Application/Common/Interfaces/IUserRepository.cs
+++ TaskTracker.Application/Common/Interfaces/IUserRepository.cs
@@ -5,9 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using TaskTracker.Application.Common.Interfaces.Base;
 using TaskTracker.Domain.Entities;
+using TaskTracker.Domain.Enums;
 
 namespace TaskTracker.Application.Common.Interfaces;
 
 public interface IUserRepository : IRepository<User>
 {
+    Task<User> GetByEmailAsync(string email);
+    Task<bool> IsEmailUniqueAsync(string email);
+    Task<IEnumerable<UserRole>> GetUserRolesAsync(Guid userId);
 }
+
