41869e231459c06b7a2894e83cce7af6276cb6ce
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-03 21:48:29 +0300

    added-base-repository

---
+++ TaskTracker.Persistence/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.Base;
+
+namespace TaskTracker.Persistence.Repositories.Base;
+
+public class BaseRepository<TEntity, TId> : IRepository<TEntity, TId>
+    where TEntity : class
+{
+    protected readonly ApplicationDbContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+    public BaseRepository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public virtual async Task AddAsync(TEntity entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteAsync(TId Id)
+    {
+        var entity = await GetByIdAsync(Id);
+        if (entity != null)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public virtual async Task<TEntity?> GetByIdAsync(TId Id)
+    {
+        return await _dbSet.FindAsync(Id);
+    }
+
+    public virtual async Task UpdateAsync(TEntity entity)
+    {
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+}
