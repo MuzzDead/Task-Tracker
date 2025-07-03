337c4984a466bb905facf9b3935e2ce551dee367
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-03 22:41:24 +0300

    added-boardrole-repository

---
+++ TaskTracker.Persistence/Repositories/BoardRoleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces;
+using TaskTracker.Domain.Entities;
+using TaskTracker.Persistence.Repositories.Base;
+
+namespace TaskTracker.Persistence.Repositories;
+
+public class BoardRoleRepository : BaseRepository<BoardRole, Guid>, IBoardRoleRepository
+{
+    public BoardRoleRepository(ApplicationDbContext context) : base(context) { }
+}
