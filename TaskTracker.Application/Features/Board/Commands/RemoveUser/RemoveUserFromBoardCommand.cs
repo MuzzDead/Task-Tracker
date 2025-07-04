3b013b99bbc41852b42ee17004fada0d148cd13e
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-04 19:58:52 +0300

    add-delete-user-command-for-board

---
+++ TaskTracker.Application/Features/Board/Commands/RemoveUser/RemoveUserFromBoardCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Application.Features.Board.Commands.RemoveUser;
+
+public class RemoveUserFromBoardCommand : IRequest
+{
+    public Guid BoardId { get; set; }
+    public Guid UserId { get; set; }
+ 
+    public RemoveUserFromBoardCommand(Guid boardId, Guid userId)
+    {
+        BoardId = boardId;
+        UserId = userId;
+    }
+}
---
+++ TaskTracker.Application/Features/Board/Commands/RemoveUser/RemoveUserFromBoardCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.Exceptions;
+
+namespace TaskTracker.Application.Features.Board.Commands.RemoveUser;
+
+public class RemoveUserFromBoardCommandHandler : IRequestHandler<RemoveUserFromBoardCommand>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+
+    public RemoveUserFromBoardCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task Handle(RemoveUserFromBoardCommand request, CancellationToken cancellationToken)
+    {
+        using var unitOfWork = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var board = await unitOfWork.Boards.GetByIdAsync(request.BoardId);
+
+        if (board == null)
+        {
+            throw new NotFoundException($"Board with ID {request.BoardId} not found");
+        }
+
+        await unitOfWork.Boards.RemoveUserAsync(request.BoardId, request.UserId);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
