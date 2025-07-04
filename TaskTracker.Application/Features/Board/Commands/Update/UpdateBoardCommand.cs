8b953b911d00e359fb1338642ff1a262fae40b8a
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-04 19:57:59 +0300

    add-update-command

---
+++ TaskTracker.Application/Features/Board/Commands/Update/UpdateBoardCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Application.Features.Board.Commands.Update;
+
+public class UpdateBoardCommand : IRequest
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public Guid UpdatedBy { get; set; }
+}
---
+++ TaskTracker.Application/Features/Board/Commands/Update/UpdateBoardCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.Exceptions;
+
+namespace TaskTracker.Application.Features.Board.Commands.Update;
+
+public class UpdateBoardCommandHandler : IRequestHandler<UpdateBoardCommand>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public UpdateBoardCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task Handle(UpdateBoardCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var board = await uow.Boards.GetByIdAsync(request.Id);
+
+        if (board == null)
+        {
+            throw new NotFoundException($"Board with ID {request.Id} not found");
+        }
+
+        board.Title = request.Title;
+        board.Description = request.Description;
+        board.UpdatedAt = DateTimeOffset.UtcNow;
+        board.UpdatedBy = request.UpdatedBy.ToString();
+
+        await uow.Boards.UpdateAsync(board);
+        await uow.SaveChangesAsync(cancellationToken);
+    }
+}
