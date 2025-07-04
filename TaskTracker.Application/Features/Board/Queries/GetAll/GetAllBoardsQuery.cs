334966ef590da7e5d1d2b77cb0d8c56d1de30f72
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-04 19:57:28 +0300

    add-get-all-query

---
+++ TaskTracker.Application/Features/Board/Queries/GetAll/GetAllBoardsQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.DTOs;
+
+namespace TaskTracker.Application.Features.Board.Queries.GetAll;
+
+public class GetAllBoardsQuery : IRequest<IEnumerable<BoardDto>>
+{
+    public bool IncludeArchived { get; set; } = false;
+}
