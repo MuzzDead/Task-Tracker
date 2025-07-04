dad77921438da23fab6b46c96f65a95d3e85ac86
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-04 16:48:21 +0300

    added-interfaces-for-unit-of-work

---
+++ TaskTracker.Application/Common/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Application.Common.Interfaces.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    IBoardRepository Boards { get; }
+    IBoardRoleRepository BoardRoles { get; }
+    ICardRepository Cards { get; }
+    IColumnRepository Columns { get; }
+    ICommentRepository Comments { get; }
+    IStateRepository States { get; }
+    IUserRepository Users { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    Task BeginTransactionAsync();
+    Task CommitTransactionAsync();
+    Task RollbackTransactionAsync();
+}
