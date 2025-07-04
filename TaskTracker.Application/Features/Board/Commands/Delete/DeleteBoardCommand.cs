88dc017cbf4034799f32577a4f4060e0183130b9
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-04 19:58:16 +0300

    add-delete-command

---
+++ TaskTracker.Application/Features/Board/Commands/Delete/DeleteBoardCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Application.Features.Board.Commands.Delete;
+
+public class DeleteBoardCommand : IRequest
+{
+    public Guid Id { get; set; }
+    public DeleteBoardCommand(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ TaskTracker.Application/Features/Board/Commands/Delete/DeleteBoardCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.Exceptions;
+
+namespace TaskTracker.Application.Features.Board.Commands.Delete;
+
+public class DeleteBoardCommandHandler : IRequestHandler<DeleteBoardCommand>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public DeleteBoardCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task Handle(DeleteBoardCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var board = await uow.Boards.GetByIdAsync(request.Id);
+
+        if (board == null)
+        {
+            throw new NotFoundException($"Board with ID {request.Id} not found");
+        }
+
+        await uow.Boards.DeleteAsync(request.Id);
+        await uow.SaveChangesAsync(cancellationToken);
+    }
+}
