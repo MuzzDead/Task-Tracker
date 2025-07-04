518fe8184553829778aaee8ba88a4f54ca0b5c62
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-04 19:59:07 +0300

    add-archive-command-for-board

---
+++ TaskTracker.Application/Features/Board/Commands/Archive/ArchiveBoardCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Application.Features.Board.Commands.Archive;
+
+public class ArchiveBoardCommand : IRequest
+{
+    public Guid Id { get; set; }
+
+    public ArchiveBoardCommand(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ TaskTracker.Application/Features/Board/Commands/Archive/ArchiveBoardCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.Exceptions;
+
+namespace TaskTracker.Application.Features.Board.Commands.Archive;
+
+public class ArchiveBoardCommandHandler : IRequestHandler<ArchiveBoardCommand>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public ArchiveBoardCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task Handle(ArchiveBoardCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var board = await uow.Boards.GetByIdAsync(request.Id);
+
+        if (board == null)
+        {
+            throw new NotFoundException($"Board with ID {request.Id} not found");
+        }
+
+        if (board.IsArchived)
+        {
+            throw new InvalidOperationException("Board is already archived");
+        }
+
+        await uow.Boards.ArchiveAsync(request.Id);
+        await uow.SaveChangesAsync(cancellationToken);
+    }
+}
