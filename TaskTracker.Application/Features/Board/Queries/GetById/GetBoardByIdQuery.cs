d570c54270edb2dc72c5fd3dd967593cb2bb1b7d
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-04 19:57:07 +0300

    add-get-by-id-query

---
+++ TaskTracker.Application/Features/Board/Queries/GetById/GetBoardByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.DTOs;
+
+namespace TaskTracker.Application.Features.Board.Queries.GetById;
+
+public class GetBoardByIdQuery : IRequest<BoardDto?>
+{
+    public Guid Id { get; set; }
+
+    public GetBoardByIdQuery(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ TaskTracker.Application/Features/Board/Queries/GetById/GetBoardByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.DTOs;
+
+namespace TaskTracker.Application.Features.Board.Queries.GetById;
+
+public class GetBoardByIdQueryHandler : IRequestHandler<GetBoardByIdQuery, BoardDto>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    private readonly IMapper _mapper;
+
+    public GetBoardByIdQueryHandler(IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+        _mapper = mapper;
+    }
+
+    public async Task<BoardDto> Handle(GetBoardByIdQuery request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var board = await uow.Boards.GetByIdAsync(request.Id);
+
+        return board == null ? null : _mapper.Map<BoardDto>(board);
+    }
+}
