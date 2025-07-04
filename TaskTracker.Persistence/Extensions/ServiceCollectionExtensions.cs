523146401c4f28a11e689a603bad1a017f7f70f6
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-04 18:21:01 +0300

    upd-service-collection-extensions

--- TaskTracker.Persistence/Extensions/ServiceCollectionExtensions.cs
+++ TaskTracker.Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TaskTracker.Application.Common.Interfaces;
 using TaskTracker.Application.Common.Interfaces.Base;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
 using TaskTracker.Persistence.Repositories;
 using TaskTracker.Persistence.Repositories.Base;
 
@@ -22,14 +23,7 @@ public static class ServiceCollectionExtensions
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
-        services.AddScoped(typeof(IRepository<,>), typeof(BaseRepository<,>));
-        services.AddScoped<IUserRepository, UserRepository>();
-        services.AddScoped<IBoardRoleRepository, BoardRoleRepository>();
-        services.AddScoped<IBoardRepository, BoardRepository>();
-        services.AddScoped<ICardRepository, CardRepository>();
-        services.AddScoped<IColumnRepository, ColumnRepository>();
-        services.AddScoped<IStateRepository, StateRepository>();
-        services.AddScoped<ICommentRepository, CommentRepository>();
+        services.AddScoped<IUnitOfWorkFactory, UnitOfWorkFactory>();
 
         return services;
     }
