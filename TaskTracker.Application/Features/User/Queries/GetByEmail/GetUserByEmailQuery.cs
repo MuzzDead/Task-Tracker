5f4142bb35444302dea1fd1cd7fe83023fb2e6b9
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 16:48:00 +0300

    added-get-by-email-query

---
+++ TaskTracker.Application/Features/User/Queries/GetByEmail/GetUserByEmailQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TaskTracker.Application.DTOs;
+
+namespace TaskTracker.Application.Features.User.Queries.GetByEmail;
+
+public class GetUserByEmailQuery : IRequest<UserDto>
+{
+    public string Email { get; set; }
+}
