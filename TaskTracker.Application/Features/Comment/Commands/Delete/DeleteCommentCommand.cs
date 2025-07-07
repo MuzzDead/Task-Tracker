17d3477d85516100954bdce79ae7929cc8798577
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 13:13:55 +0300

    removed-constructor-from-delete-command

--- TaskTracker.Application/Features/Comment/Commands/Delete/DeleteCommentCommand.cs
+++ TaskTracker.Application/Features/Comment/Commands/Delete/DeleteCommentCommand.cs
@@ -5,9 +5,4 @@ namespace TaskTracker.Application.Features.Comment.Commands.Delete;
 public class DeleteCommentCommand : IRequest
 {
     public Guid Id { get; set; }
-
-    public DeleteCommentCommand(Guid id)
-    {
-        Id = id;
-    }
 }
