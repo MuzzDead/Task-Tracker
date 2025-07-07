aba95ecf054b5dfb597dd9e674e9740cea7fc31c
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 12:15:25 +0300

    added-get-by-columnid-query

---
+++ TaskTracker.Application/Features/Card/Queries/GetByColumnId/GetCardsByColumnIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.DTOs;
+
+namespace TaskTracker.Application.Features.Card.Queries.GetByColumnId;
+
+public class GetCardsByColumnIdQuery : IRequest<IEnumerable<CardDto>>
+{
+    public Guid ColumnId { get; set; }
+
+    public GetCardsByColumnIdQuery(Guid columnId)
+    {
+        ColumnId = columnId;
+    }
+}
