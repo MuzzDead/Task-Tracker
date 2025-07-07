f24c49febcefff054cf91cea5be191d988f1f11b
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 16:17:17 +0300

    added-create-command

---
+++ TaskTracker.Application/Features/User/Command/Create/CreateUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TaskTracker.Application.Features.User.Command.Create;
+
+public class CreateUserCommand : IRequest<Guid>
+{
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+}
