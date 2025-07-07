9b930aadccddbb81c8210a0d1898abc0472f905c
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 13:00:19 +0300

    added-update-command

---
+++ TaskTracker.Application/Features/Comment/Commands/Update/UpdateCommentCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TaskTracker.Application.Features.Comment.Commands.Update;
+
+public class UpdateCommentCommand : IRequest
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public Guid UpdatedBy { get; set; }
+}
