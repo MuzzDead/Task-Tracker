44beeaa19923ac77d5b0c53ba254bc5f0f821a66
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 12:03:23 +0300

    added-delete-command

---
+++ TaskTracker.Application/Features/Card/Commands/Delete/DeleteCardCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Application.Features.Card.Commands.Delete;
+
+public class DeleteCardCommand : IRequest
+{
+    public Guid Id { get; set; }
+
+    public DeleteCardCommand(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ TaskTracker.Application/Features/Card/Commands/Delete/DeleteCardCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.Exceptions;
+
+namespace TaskTracker.Application.Features.Card.Commands.Delete;
+
+public class DeleteCardCommandHandler : IRequestHandler<DeleteCardCommand>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public DeleteCardCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task Handle(DeleteCardCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var card = await uow.Cards.GetByIdAsync(request.Id);
+
+        if (card == null)
+        {
+            throw new NotFoundException($"Card with ID {request.Id} not found");
+        }
+
+        await uow.Cards.DeleteAsync(request.Id);
+        await uow.SaveChangesAsync(cancellationToken);
+    }
+}
