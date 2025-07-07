73b1f58fbf1b199324c35f9bb4a820c6d7145e89
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 14:52:45 +0300

    added-update-command

---
+++ TaskTracker.Application/Features/Column/Commands/Update/UpdateColumnCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TaskTracker.Application.Features.Column.Commands.Update;
+
+public class UpdateColumnCommand : IRequest
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public int ColumnIndex { get; set; }
+}
