d0727e240794b39a848bc4486d6ccd18d900de07
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 18:15:01 +0300

    added-delete-command

---
+++ TaskTracker.Application/Features/State/Command/Delete/DeleteStateCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace TaskTracker.Application.Features.State.Command.Delete;
+
+public class DeleteStateCommand : IRequest
+{
+    public Guid Id { get; set; }
+}
---
+++ TaskTracker.Application/Features/State/Command/Delete/DeleteStateCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.Exceptions;
+
+namespace TaskTracker.Application.Features.State.Command.Delete;
+
+public class DeleteStateCommandHandler : IRequestHandler<DeleteStateCommand>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public DeleteStateCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task Handle(DeleteStateCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var state = await uow.States.GetByIdAsync(request.Id);
+        if (state == null)
+        {
+            throw new NotFoundException($"User with ID {request.Id} was not found.");
+        }
+
+        await uow.States.DeleteAsync(request.Id);
+        await uow.SaveChangesAsync(cancellationToken);
+    }
+}
