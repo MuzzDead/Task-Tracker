a8bb7a65ba85f2ff732e824fa1a010eb03a3eb3c
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 13:18:22 +0300

    added-get-by-id-query

---
+++ TaskTracker.Application/Features/Comment/Queries/GetById/GetCommentByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TaskTracker.Application.DTOs;
+
+namespace TaskTracker.Application.Features.Comment.Queries.GetById;
+
+public class GetCommentByIdQuery : IRequest<CommentDto?>
+{
+    public Guid Id { get; set; }
+}
---
+++ TaskTracker.Application/Features/Comment/Queries/GetById/GetCommentByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.DTOs;
+
+namespace TaskTracker.Application.Features.Comment.Queries.GetById;
+
+public class GetCommentByIdQueryHandler : IRequestHandler<GetCommentByIdQuery, CommentDto>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    private readonly IMapper _mapper;
+
+    public GetCommentByIdQueryHandler(IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+        _mapper = mapper;
+    }
+
+    public async Task<CommentDto> Handle(GetCommentByIdQuery request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var comment = await uow.Comments.GetByIdAsync(request.Id);
+
+        return comment == null ? null : _mapper.Map<CommentDto>(comment);
+    }
+}
