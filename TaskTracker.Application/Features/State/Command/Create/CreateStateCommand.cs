d5623282b654befe0073895a1671086a12b9c28a
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 17:57:04 +0300

    added-create-command

---
+++ TaskTracker.Application/Features/State/Command/Create/CreateStateCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using TaskTracker.Domain.Enums;
+
+namespace TaskTracker.Application.Features.State.Command.Create;
+
+public class CreateStateCommand : IRequest<Guid>
+{
+    public string Description { get; set; } = string.Empty;
+    public Status Status { get; set; } = Status.Pending;
+    public Priority Priority { get; set; } = Priority.Medium;
+    public Guid CardId { get; set; }
+}
