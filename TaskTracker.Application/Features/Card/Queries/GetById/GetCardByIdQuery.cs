b2217ebff4516e0ae3c45764a1310a77d48433f9
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 12:15:07 +0300

    added-get-by-id-query

---
+++ TaskTracker.Application/Features/Card/Queries/GetById/GetCardByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.DTOs;
+
+namespace TaskTracker.Application.Features.Card.Queries.GetById;
+
+public class GetCardByIdQuery : IRequest<CardDto>
+{
+    public Guid Id { get; set; }
+    public GetCardByIdQuery(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ TaskTracker.Application/Features/Card/Queries/GetById/GetCardByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.DTOs;
+
+namespace TaskTracker.Application.Features.Card.Queries.GetById;
+
+public class GetCardByIdQueryHandler : IRequestHandler<GetCardByIdQuery, CardDto>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    private readonly IMapper _mapper;
+
+    public GetCardByIdQueryHandler(IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+        _mapper = mapper;
+    }
+
+    public async Task<CardDto> Handle(GetCardByIdQuery request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var card = await uow.Cards.GetByIdAsync(request.Id);
+
+        return card == null ? null : _mapper.Map<CardDto>(card);
+    }
+}
