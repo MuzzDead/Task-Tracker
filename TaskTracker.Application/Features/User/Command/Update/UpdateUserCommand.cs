a608008d270e42142b9a55741765ef43fb527546
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 16:22:00 +0300

    added-update-command

---
+++ TaskTracker.Application/Features/User/Command/Update/UpdateUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TaskTracker.Application.Features.User.Command.Update;
+
+public class UpdateUserCommand : IRequest
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
