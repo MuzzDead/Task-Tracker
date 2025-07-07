faad1b4518f8cf8e3f01689f1a7574fe1c37dac3
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 13:03:27 +0300

    added-delete-command

---
+++ TaskTracker.Application/Features/Comment/Commands/Delete/DeleteCommentCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace TaskTracker.Application.Features.Comment.Commands.Delete;
+
+public class DeleteCommentCommand : IRequest
+{
+    public Guid Id { get; set; }
+
+    public DeleteCommentCommand(Guid id)
+    {
+        Id = id;
+    }
+}
