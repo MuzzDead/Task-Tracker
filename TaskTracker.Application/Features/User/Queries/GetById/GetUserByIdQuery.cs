c0fe0b1441902356606c3b71145783083a9931c2
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 16:41:38 +0300

    added-get-by-id-query

---
+++ TaskTracker.Application/Features/User/Queries/GetById/GetUserByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TaskTracker.Application.DTOs;
+
+namespace TaskTracker.Application.Features.User.Queries.GetById;
+
+public class GetUserByIdQuery : IRequest<UserDto>
+{
+    public Guid Id { get; set; }
+}
---
+++ TaskTracker.Application/Features/User/Queries/GetById/GetUserByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.DTOs;
+using TaskTracker.Application.Exceptions;
+
+
+namespace TaskTracker.Application.Features.User.Queries.GetById;
+
+public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserDto>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    private readonly IMapper _mapper;
+    public GetUserByIdQueryHandler(IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+        _mapper = mapper;
+    }
+
+    public async Task<UserDto> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var user = await uow.Users.GetByIdAsync(request.Id);
+        if (user == null)
+            throw new NotFoundException($"User with ID {request.Id} was not found.");
+
+        return _mapper.Map<UserDto>(user);
+    }
+}
