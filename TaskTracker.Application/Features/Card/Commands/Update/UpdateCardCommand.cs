5bc7014b43cf20ca0acb589997625123fcc6ab50
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 11:44:02 +0300

    added-update-command

---
+++ TaskTracker.Application/Features/Card/Commands/Update/UpdateCardCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Application.Features.Card.Commands.Update;
+
+public class UpdateCardCommand : IRequest
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public Guid ColumnId { get; set; }
+    public int RowIndex { get; set; }
+    public Guid UpdatedBy { get; set; }
+}
---
+++ TaskTracker.Application/Features/Card/Commands/Update/UpdateCardCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.Exceptions;
+
+namespace TaskTracker.Application.Features.Card.Commands.Update;
+
+public class UpdateCardCommandHandler : IRequestHandler<UpdateCardCommand>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+
+    public UpdateCardCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task Handle(UpdateCardCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var card = await uow.Cards.GetByIdAsync(request.Id);
+
+        if (card == null)
+        {
+            throw new NotFoundException($"Card with ID {request.Id} not found");
+        }
+
+        card.Title = request.Title;
+        card.ColumnId = request.ColumnId;
+        card.RowIndex = request.RowIndex;
+        card.UpdatedAt = DateTimeOffset.UtcNow;
+        card.UpdatedBy = request.UpdatedBy.ToString();
+
+        await uow.Cards.UpdateAsync(card);
+        await uow.SaveChangesAsync(cancellationToken);
+    }
+}
