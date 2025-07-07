2066f6524f9c6814a7478fe3b754b96ec24ab024
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 11:43:44 +0300

    added-create-command

---
+++ TaskTracker.Application/Features/Card/Commands/Create/CreateCardCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Application.Features.Card.Commands.Create;
+
+public class CreateCardCommand : IRequest<Guid>
+{
+    public string Title { get; set; } = string.Empty;
+    public Guid ColumnId { get; set; }
+    public int RowIndex { get; set; }
+    public Guid CreatedBy { get; set; }
+}
---
+++ TaskTracker.Application/Features/Card/Commands/Create/CreateCardCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Domain.Entities;
+
+namespace TaskTracker.Application.Features.Card.Commands.Create;
+
+public class CreateCardCommandHandler : IRequestHandler<CreateCardCommand, Guid>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public CreateCardCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task<Guid> Handle(CreateCardCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var card = new Domain.Entities.Card
+        {
+            Title = request.Title,
+            ColumnId = request.ColumnId,
+            RowIndex = request.RowIndex,
+            CreatedBy = request.CreatedBy.ToString()
+        };
+
+        await uow.Cards.AddAsync(card);
+        await uow.SaveChangesAsync(cancellationToken);
+
+        return card.Id;
+    }
+}
