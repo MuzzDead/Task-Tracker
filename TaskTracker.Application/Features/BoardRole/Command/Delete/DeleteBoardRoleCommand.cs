0fd5c48c8c97138f6da0e104bfea4a00af435098
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 19:10:55 +0300

    added-delete-command

---
+++ TaskTracker.Application/Features/BoardRole/Command/Delete/DeleteBoardRoleCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace TaskTracker.Application.Features.BoardRole.Command.Delete;
+
+public class DeleteBoardRoleCommand : IRequest
+{
+    public Guid Id { get; set; }
+}
---
+++ TaskTracker.Application/Features/BoardRole/Command/Delete/DeleteBoardRoleCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.Exceptions;
+
+namespace TaskTracker.Application.Features.BoardRole.Command.Delete;
+
+public class DeleteBoardRoleCommandHandler : IRequestHandler<DeleteBoardRoleCommand>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public DeleteBoardRoleCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task Handle(DeleteBoardRoleCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var role = await uow.BoardRoles.GetByIdAsync(request.Id);
+        if (role == null)
+        {
+            throw new NotFoundException($"BoardRole with ID {request.Id} was not found.");
+        }
+
+        await uow.BoardRoles.DeleteAsync(request.Id);
+        await uow.SaveChangesAsync();
+    }
+}
