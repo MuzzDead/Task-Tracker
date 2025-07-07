582caf6b58f415a8341e0d3b9998aaa70d9c57c5
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 19:04:47 +0300

    added-update-command

---
+++ TaskTracker.Application/Features/BoardRole/Command/Update/UpdateBoardRoleQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TaskTracker.Domain.Enums;
+
+namespace TaskTracker.Application.Features.BoardRole.Command.Update;
+
+public class UpdateBoardRoleQuery : IRequest
+{
+    public Guid Id { get; set; }
+    public UserRole Role { get; set; }
+}
---
+++ TaskTracker.Application/Features/BoardRole/Command/Update/UpdateBoardRoleQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.Exceptions;
+
+namespace TaskTracker.Application.Features.BoardRole.Command.Update;
+
+public class UpdateBoardRoleQueryHandler : IRequestHandler<UpdateBoardRoleQuery>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public UpdateBoardRoleQueryHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task Handle(UpdateBoardRoleQuery request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var role = await uow.BoardRoles.GetByIdAsync(request.Id);
+        if (role == null)
+        {
+            throw new NotFoundException($"BoardRole with ID {request.Id} was not found.");
+        }
+
+        await uow.BoardRoles.UpdateAsync(role);
+        await uow.SaveChangesAsync();
+    }
+}
