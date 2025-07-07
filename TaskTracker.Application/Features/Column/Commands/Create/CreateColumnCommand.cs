db82621e74d9a79a18ed60ec35e6a72506d63066
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 14:47:23 +0300

    added-create-comand

---
+++ TaskTracker.Application/Features/Column/Commands/Create/CreateColumnCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TaskTracker.Application.Features.Column.Commands.Create;
+
+public class CreateColumnCommand : IRequest<Guid>
+{
+    public string Title { get; set; } = string.Empty;
+    public Guid BoardId { get; set; }
+    public int ColumnIndex { get; set; }
+}
---
+++ TaskTracker.Application/Features/Column/Commands/Create/CreateColumnCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+
+namespace TaskTracker.Application.Features.Column.Commands.Create;
+
+public class CreateColumnCommandHandler : IRequestHandler<CreateColumnCommand, Guid>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public CreateColumnCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task<Guid> Handle(CreateColumnCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var column = new Domain.Entities.Column
+        {
+            Title = request.Title,
+            BoardId = request.BoardId,
+            ColumnIndex = request.ColumnIndex,
+            CreatedAt = DateTimeOffset.UtcNow,
+            CreatedBy = "system"
+        };
+
+        await uow.Columns.AddAsync(column);
+        await uow.SaveChangesAsync(cancellationToken);
+
+        return column.Id;
+    }
+}
