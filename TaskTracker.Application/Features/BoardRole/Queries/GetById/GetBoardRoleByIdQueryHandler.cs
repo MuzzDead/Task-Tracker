8a3566df6fbb8d2a7e896263faac603841e6c250
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 19:23:52 +0300

    added-get-by-id-query

---
+++ TaskTracker.Application/Features/BoardRole/Queries/GetById/GetBoardRoleByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.DTOs;
+using TaskTracker.Application.Exceptions;
+
+namespace TaskTracker.Application.Features.BoardRole.Queries.GetById;
+
+public class GetBoardRoleByIdQueryHandler : IRequestHandler<GetBoardRoleByIdQuery, BoardRoleDto>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    private readonly IMapper _mapper;
+    public GetBoardRoleByIdQueryHandler(IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+        _mapper = mapper;
+    }
+
+    public async Task<BoardRoleDto> Handle(GetBoardRoleByIdQuery request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var role = await uow.BoardRoles.GetByIdAsync(request.Id);
+        if (role == null)
+        {
+            throw new NotFoundException($"BoardRole with ID {request.Id} was not found.");
+        }
+
+        return _mapper.Map<BoardRoleDto>(role);
+    }
+}
