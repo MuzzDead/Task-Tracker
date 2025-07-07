5934c20039dabdd896d122329f64995b28ed8ef1
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 18:58:52 +0300

    added-create-command

---
+++ TaskTracker.Application/Features/BoardRole/Command/Create/CreateBoardRoleCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TaskTracker.Domain.Enums;
+
+namespace TaskTracker.Application.Features.BoardRole.Command.Create;
+
+public class CreateBoardRoleCommand : IRequest<Guid>
+{
+    public Guid UserId { get; set; }
+    public Guid BoardId { get; set; }
+    public UserRole Role { get; set; }
+}
---
+++ TaskTracker.Application/Features/BoardRole/Command/Create/CreateBoardRoleCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+
+namespace TaskTracker.Application.Features.BoardRole.Command.Create;
+
+public class CreateBoardRoleCommandHandler : IRequestHandler<CreateBoardRoleCommand, Guid>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public CreateBoardRoleCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task<Guid> Handle(CreateBoardRoleCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var role = new Domain.Entities.BoardRole
+        {
+            UserId = request.UserId,
+            BoardId = request.BoardId,
+            Role = request.Role
+        };
+
+        await uow.BoardRoles.AddAsync(role);
+        await uow.SaveChangesAsync(cancellationToken);
+
+        return role.Id;
+    }
+}
