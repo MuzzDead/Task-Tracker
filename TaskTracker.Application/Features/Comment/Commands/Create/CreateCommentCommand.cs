ad03852242d976c68624031eea457b9da03ac9e1
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 12:57:42 +0300

    added-create-command

---
+++ TaskTracker.Application/Features/Comment/Commands/Create/CreateCommentCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Application.Features.Comment.Commands.Create;
+
+public class CreateCommentCommand : IRequest<Guid>
+{
+    public string Text { get; set; } = string.Empty;
+    public Guid CardId { get; set; }
+    public Guid UserId { get; set; }
+}
---
+++ TaskTracker.Application/Features/Comment/Commands/Create/CreateCommentCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+
+namespace TaskTracker.Application.Features.Comment.Commands.Create;
+
+public class CreateCommentCommandHandler : IRequestHandler<CreateCommentCommand, Guid>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+
+    public CreateCommentCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task<Guid> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var comment = new Domain.Entities.Comment
+        {
+            Text = request.Text,
+            CardId = request.CardId,
+            UserId = request.UserId,
+            CreatedBy = request.UserId.ToString()
+        };
+
+        await uow.Comments.AddAsync(comment);
+        await uow.SaveChangesAsync(cancellationToken);
+
+        return comment.Id;
+    }
+}
