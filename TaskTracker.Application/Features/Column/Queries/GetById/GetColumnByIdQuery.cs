b8ca7f1f3cbe6f015820e45683e7781f059ee7bc
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 15:02:46 +0300

    added-get-by-id-query

---
+++ TaskTracker.Application/Features/Column/Queries/GetById/GetColumnByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.DTOs;
+
+namespace TaskTracker.Application.Features.Column.Queries.GetById;
+
+public class GetColumnByIdQuery : IRequest<ColumnDto>
+{
+    public Guid Id { get; set; }
+}
---
+++ TaskTracker.Application/Features/Column/Queries/GetById/GetColumnByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.DTOs;
+using TaskTracker.Domain.Entities;
+
+namespace TaskTracker.Application.Features.Column.Queries.GetById;
+
+public class GetColumnByIdQueryHandler : IRequestHandler<GetColumnByIdQuery, ColumnDto>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    private readonly IMapper _mapper;
+    public GetColumnByIdQueryHandler(IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+        _mapper = mapper;
+    }
+
+    public async Task<ColumnDto> Handle(GetColumnByIdQuery request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var column = await uow.Columns.GetByIdAsync(request.Id);
+
+        return column == null ? null : _mapper.Map<ColumnDto>(column);
+    }
+}
