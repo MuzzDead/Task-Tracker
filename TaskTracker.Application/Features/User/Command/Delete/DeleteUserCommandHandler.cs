d98c19498e7c203783ca3af06905cce5d92287f9
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 16:32:06 +0300

    added-delete-command

---
+++ TaskTracker.Application/Features/User/Command/Delete/DeleteUserCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using TaskTracker.Application.Common.Interfaces.UnitOfWork;
+using TaskTracker.Application.Exceptions;
+
+namespace TaskTracker.Application.Features.User.Command.Delete;
+
+public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand>
+{
+    private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+    public DeleteUserCommandHandler(IUnitOfWorkFactory unitOfWorkFactory)
+    {
+        _unitOfWorkFactory = unitOfWorkFactory;
+    }
+
+    public async Task Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    {
+        using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+
+        var user = await uow.Users.GetByIdAsync(request.Id);
+
+        if (user == null)
+        {
+            throw new NotFoundException($"Columns with ID {request.Id} not found");
+        }
+
+        await uow.Users.DeleteAsync(request.Id);
+        await uow.SaveChangesAsync();
+    }
+}
