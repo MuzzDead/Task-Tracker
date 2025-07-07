3da26651df770231b4f8619f2d13a8a6ecfb1ba0
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-07 16:50:35 +0300

    remove-usings

--- TaskTracker.Application/Exceptions/ConflictException.cs
+++ TaskTracker.Application/Exceptions/ConflictException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TaskTracker.Application.Exceptions;
+﻿namespace TaskTracker.Application.Exceptions;
 
 public class ConflictException : Exception
 {
