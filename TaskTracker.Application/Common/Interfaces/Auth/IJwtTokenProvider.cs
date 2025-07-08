f81186965d8cb121a66872cc702fd877fd53bc34
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-08 13:16:56 +0300

    added-interfaces

---
+++ TaskTracker.Application/Common/Interfaces/Auth/IJwtTokenProvider.cs
@@ -0,0 +1,8 @@
+﻿using TaskTracker.Domain.Entities;
+
+namespace TaskTracker.Application.Common.Interfaces.Auth;
+
+public interface IJwtTokenProvider
+{
+    Task<string> GenerateTokenAsync(User user);
+}
---
+++ TaskTracker.Application/Common/Interfaces/Auth/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskTracker.Application.Common.Interfaces.Auth;
+
+public interface IPasswordHasher
+{
+    string Generate(string password);
+    bool Verify(string password, string hashedPassword);
+}
