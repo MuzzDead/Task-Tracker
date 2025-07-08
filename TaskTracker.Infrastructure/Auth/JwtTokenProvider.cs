03c119287f67c834f5360da2bf0f722e01c3bde6
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-08 15:24:03 +0300

    implemented-jwt-interfaces

---
+++ TaskTracker.Infrastructure/Auth/JwtTokenProvider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TaskTracker.Application.Common.Interfaces.Auth;
+using TaskTracker.Domain.Entities;
+using TaskTracker.Domain.Options;
+
+namespace TaskTracker.Infrastructure.Auth;
+
+public class JwtTokenProvider : IJwtTokenProvider
+{
+    private readonly JwtOptions _jwtOptions;
+    public JwtTokenProvider(IOptions<JwtOptions> jwtOptions)
+    {
+        _jwtOptions = jwtOptions.Value;
+    }
+
+    public string GenerateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new(ClaimTypes.Email, user.Email),
+            new(ClaimTypes.Name, user.Username)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _jwtOptions.Issuer,
+            audience: _jwtOptions.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(2),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
---
+++ TaskTracker.Infrastructure/Auth/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using TaskTracker.Application.Common.Interfaces.Auth;
+
+namespace TaskTracker.Infrastructure.Auth;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string Generate(string password)
+    {
+        return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+    }
+
+    public bool Verify(string password, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPasswordkd);
+    }
+}
