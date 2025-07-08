d60be21503f290645954ab83692830e3fa94664d
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-08 15:40:40 +0300

    registered-services-for-jwt

---
+++ TaskTracker.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TaskTracker.Application.Common.Interfaces.Auth;
+using TaskTracker.Domain.Options;
+using TaskTracker.Infrastructure.Auth;
+
+namespace TaskTracker.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<JwtOptions>(options
+           => configuration.GetSection("JwtOptions").Bind(options));
+
+        var jwtOptions = configuration
+            .GetSection("JwtOptions")
+            .Get<JwtOptions>()!;
+
+        var secretKey = Encoding.UTF8.GetBytes(jwtOptions.SecretKey);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+        {
+            options.RequireHttpsMetadata = true;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtOptions.Issuer,
+                ValidateAudience = true,
+                ValidAudience = jwtOptions.Audience,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(secretKey)
+            };
+    });
+
+        services.AddAuthorization();
+
+        services.AddScoped<IJwtTokenProvider, JwtTokenProvider>();
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+        return services;
+    }
+}
