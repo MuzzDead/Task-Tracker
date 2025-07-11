54ceeb2286437fc9bb91c0b2556c1011deca87c3
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-11 13:45:25 +0300

    upd-create-board-method

--- TaskTracker.Application/Features/Board/Commands/Create/CreateBoardCommandHandler.cs
+++ TaskTracker.Application/Features/Board/Commands/Create/CreateBoardCommandHandler.cs
@@ -14,17 +14,37 @@ public class CreateBoardCommandHandler : IRequestHandler<CreateBoardCommand, Gui
     public async Task<Guid> Handle(CreateBoardCommand request, CancellationToken cancellationToken)
     {
         using var uow = _unitOfWorkFactory.CreateUnitOfWork();
+        await uow.BeginTransactionAsync();
 
-        var board = new Domain.Entities.Board
+        try
         {
-            Title = request.Title,
-            Description = request.Description,
-            CreatedBy = request.UserId.ToString()
-        };
+            var board = new Domain.Entities.Board
+            {
+                Title = request.Title,
+                Description = request.Description,
+                CreatedBy = request.UserId.ToString()
+            };
 
-        await uow.Boards.CreateAsync(board, request.UserId, request.UserRole);
-        await uow.SaveChangesAsync();
+            await uow.Boards.CreateAsync(board, request.UserId, request.UserRole);
+            await uow.SaveChangesAsync();
 
-        return board.Id;
+            var boardRole = new Domain.Entities.BoardRole
+            {
+                BoardId = board.Id,
+                UserId = request.UserId,
+                Role = request.UserRole
+            };
+
+            await uow.BoardRoles.AddAsync(boardRole);
+            await uow.SaveChangesAsync();
+
+            await uow.CommitTransactionAsync();
+            return board.Id;
+        }
+        catch (Exception ex)
+        {
+            await uow.RollbackTransactionAsync();
+            throw;
+        }
     }
 }
