29a475e576c763b29b9d2aff2bc670924cde0c1d
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-11 15:00:27 +0300

    upd-updb-migration

---
+++ TaskTracker.Database/DatabaseMigrator.cs
@@ -0,0 +1,36 @@
+﻿using DbUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Database;
+
+public static class DatabaseMigrator
+{
+    public static void MigrateDatabase(string connectionString)
+    {
+        var upgrader = DeployChanges.To
+            .SqlDatabase(connectionString)
+            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+            .LogToConsole()
+            .Build();
+
+        var result = upgrader.PerformUpgrade();
+
+        if (!result.Successful)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Migration failed:");
+            Console.WriteLine(result.Error);
+            Console.ResetColor();
+            throw new Exception("Database migration failed", result.Error);
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Database successfully migrated.");
+        Console.ResetColor();
+    }
+}
