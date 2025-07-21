123430cceef78031a4d74f9db40557d13d7091bc
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-21 13:17:13 +0300

    splited-column-components

---
+++ TaskTracker.Client/Components/Columns/ColumnCard.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using TaskTracker.Client.DTOs.Card;
+using TaskTracker.Client.DTOs.Column;
+
+namespace TaskTracker.Client.Components.Columns;
+
+public partial class ColumnCard : ComponentBase
+{
+    [Parameter, EditorRequired] public ColumnDto Column { get; set; } = default!;
+    [Parameter] public List<CardDto> Cards { get; set; } = new();
+    [Parameter] public bool IsLoading { get; set; }
+    [Parameter] public EventCallback<CardDto> OnCardClick { get; set; }
+    [Parameter] public EventCallback<(string title, Guid columnId)> OnAddCard { get; set; }
+
+    private async Task HandleAddCard(string title)
+    {
+        await OnAddCard.InvokeAsync((title, Column.Id));
+    }
+}
