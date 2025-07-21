52362889a99f8397ca33d96006923862d09acfee
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-21 13:06:59 +0300

    splited-card-components

---
+++ TaskTracker.Client/Components/Cards/TaskCard.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using TaskTracker.Client.DTOs.Card;
+
+namespace TaskTracker.Client.Components.Cards;
+
+public partial class TaskCard : ComponentBase
+{
+    [Parameter, EditorRequired] public CardDto Card { get; set; } = default!;
+    [Parameter] public EventCallback<CardDto> OnCardClick { get; set; }
+    [Parameter] public EventCallback<(CardDto card, DragEventArgs args)> OnDragStart { get; set; }
+}
---
+++ TaskTracker.Client/Components/Cards/TaskCardList.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using TaskTracker.Client.DTOs.Card;
+
+namespace TaskTracker.Client.Components.Cards;
+
+public partial class TaskCardList : ComponentBase
+{
+    [Parameter] public List<CardDto> Cards { get; set; } = new();
+    [Parameter] public EventCallback<CardDto> OnCardClick { get; set; }
+    [Parameter] public EventCallback<string> OnAddCard { get; set; }
+
+    private bool IsAdding;
+    private string newTitle = string.Empty;
+    private ElementReference newCardInputRef;
+
+    private async Task Start()
+    {
+        IsAdding = true;
+        newTitle = string.Empty;
+        StateHasChanged();
+
+        await Task.Yield();
+        await newCardInputRef.FocusAsync();
+    }
+
+    private Task Cancel()
+    {
+        IsAdding = false;
+        newTitle = string.Empty;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    private async Task Save()
+    {
+        if (!string.IsNullOrWhiteSpace(newTitle))
+        {
+            await OnAddCard.InvokeAsync(newTitle.Trim());
+            await Cancel();
+        }
+    }
+
+    private async Task HandleKeyPress(KeyboardEventArgs e)
+    {
+        if (e.Key == "Enter")
+            await Save();
+        else if (e.Key == "Escape")
+            await Cancel();
+    }
+}
