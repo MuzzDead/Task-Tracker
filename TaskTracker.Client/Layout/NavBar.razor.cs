7c1e78a1ec3dacb30ae1c20adc588d6cea483593
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-21 13:56:14 +0300

    split-layout

---
+++ TaskTracker.Client/Layout/NavBar.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TaskTracker.Client.Layout;
+
+public partial class NavBar : ComponentBase
+{
+    private bool isAuthenticated = true;
+    private string userDisplayName = "Test User";
+
+    private void NavigateTo(string path)
+    {
+        Navigation.NavigateTo(path);
+    }
+
+    private bool IsActive(string path)
+    {
+        var currentPath = Navigation.Uri.Replace(Navigation.BaseUri, "/");
+        return currentPath.StartsWith(path, StringComparison.OrdinalIgnoreCase);
+    }
+}
