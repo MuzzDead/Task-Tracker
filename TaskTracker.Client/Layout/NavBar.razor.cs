6802755432af6f56c4b91848fab4af891c2e90d9
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-22 21:52:28 +0300

    upd-navbar-with-adding-logout

--- TaskTracker.Client/Layout/NavBar.razor.cs
+++ TaskTracker.Client/Layout/NavBar.razor.cs
@@ -1,11 +1,41 @@
 ﻿using Microsoft.AspNetCore.Components;
+using TaskTracker.Client.Services.Interfaces;
 
 namespace TaskTracker.Client.Layout;
 
 public partial class NavBar : ComponentBase
 {
-    private bool isAuthenticated = true;
-    private string userDisplayName = "Test User";
+    [Inject] private NavigationManager Navigation { get; set; } = default!;
+    [Inject] private IAuthStateService AuthStateService { get; set; } = default!;
+
+    private bool showLogout = false;
+    private bool isAuthenticated = false;
+    private string userDisplayName = string.Empty;
+
+    private void ToggleLogout()
+    {
+        if (isAuthenticated)
+        {
+            showLogout = !showLogout;
+        }
+    }
+
+    private async Task LogoutAsync()
+    {
+        await AuthStateService.ClearAuthDataAsync();
+        isAuthenticated = false;
+        userDisplayName = string.Empty;
+        showLogout = false;
+        Navigation.NavigateTo("/");
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await AuthStateService.InitializeAsync();
+
+        isAuthenticated = AuthStateService.IsAuthenticated;
+        userDisplayName = AuthStateService.CurrentUser?.Username ?? string.Empty;
+    }
 
     private void NavigateTo(string path)
     {
