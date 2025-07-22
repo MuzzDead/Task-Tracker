57aa15a8ded645b9fa85d5bea8d50aed1955a157
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-22 21:22:17 +0300

    added-dtos

---
+++ TaskTracker.Client/DTOs/Auth/AuthResponse.cs
@@ -0,0 +1,9 @@
+﻿using TaskTracker.Client.DTOs.User;
+
+namespace TaskTracker.Client.DTOs.Auth;
+
+public class AuthResponse
+{
+    public string Token { get; set; } = string.Empty;
+    public UserDto User { get; set; } = null!;
+}
---
+++ TaskTracker.Client/DTOs/User/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTracker.Client.DTOs.User;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+}
