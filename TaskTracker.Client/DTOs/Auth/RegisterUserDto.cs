259bf76d38b682512ea39f428eb59c4dd8dda08b
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-22 21:55:54 +0300

    upd-register-dto

--- TaskTracker.Client/DTOs/Auth/RegisterUserDto.cs
+++ TaskTracker.Client/DTOs/Auth/RegisterUserDto.cs
@@ -1,8 +1,15 @@
-﻿namespace TaskTracker.Client.DTOs.Auth;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracker.Client.DTOs.Auth;
 
 public class RegisterUserDto
 {
+    [Required]
     public string Username { get; set; } = string.Empty;
+
+    [Required]
     public string Email { get; set; } = string.Empty;
+    
+    [Required]    
     public string Password { get; set; } = string.Empty;
 }
