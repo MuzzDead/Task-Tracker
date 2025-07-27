d9467f5a7c0a03a48bb601d6e914946982be68ae
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-27 18:46:18 +0300

    added-comment-service

---
+++ TaskTracker.Client/Services/Interfaces/ICommentService.cs
@@ -0,0 +1,26 @@
+﻿using Refit;
+using TaskTracker.Client.DTOs.Comment;
+
+namespace TaskTracker.Client.Services.Interfaces;
+
+public interface ICommentService
+{
+    [Get("/comment/{id}")]
+    Task<CommentDto> GetByIdAsync(Guid id);
+
+
+    [Get("/comment/card/{cardId}")]
+    Task<List<CommentDto>> GetByCardIdAsync(Guid cardId);
+
+    
+    [Post("/comment")]
+    Task<Guid> CreateAsync([Body] CreateCommentDto command);
+
+    
+    [Put("/comment/{id}")]
+    Task UpdateAsync(Guid id, [Body] UpdateCommentDto command);
+
+    
+    [Delete("/comment/{id}")]
+    Task DeleteAsync(Guid id);
+}
