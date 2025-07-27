b81bf845c5e42c300ca981cf55792da49c567a47
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-27 18:45:41 +0300

    add-comment-dtos

---
+++ TaskTracker.Client/DTOs/Comment/CreateCommentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTracker.Client.DTOs.Comment;
+
+public class CreateCommentDto
+{
+    public string Text { get; set; }
+    public Guid CardId { get; set; }
+    public Guid UserId { get; set; }
+}
