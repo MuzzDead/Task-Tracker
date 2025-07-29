5289b962cb09cb577d14230ba5fc44a338c49977
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-29 19:08:11 +0300

    added-created-by

--- TaskTracker.Application/Features/Comment/Commands/Create/CreateCommentCommand.cs
+++ TaskTracker.Application/Features/Comment/Commands/Create/CreateCommentCommand.cs
@@ -7,4 +7,5 @@ public class CreateCommentCommand : IRequest<Guid>
     public string Text { get; set; } = string.Empty;
     public Guid CardId { get; set; }
     public Guid UserId { get; set; }
+    public string CreatedBy { get; set; } = string.Empty;
 }
