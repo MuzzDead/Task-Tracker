248ce3f4877171b976e891ce6ff1e3750903dde4
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-30 16:39:09 +0300

    added-user-service

---
+++ TaskTracker.Client/DTOs/User/UpdateUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTracker.Client.DTOs.User;
+
+public class UpdateUserDto
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+}
