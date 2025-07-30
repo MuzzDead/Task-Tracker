80cefe0e288d50d09db1588e5d6c54497faaa07d
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-07-30 19:05:38 +0300

    added-change-password-page

---
+++ TaskTracker.Client/DTOs/User/ChangePasswordDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTracker.Client.DTOs.User;
+
+public class ChangePasswordDto
+{
+    public Guid Id { get; set; }
+    public string CurrentPassword { get; set; }
+    public string NewPassword { get; set; }
+}
