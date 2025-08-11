b8098557dfc099d6e5cc65e361bb563823afc185
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-08-11 17:12:38 +0300

    refactored-board-details

--- TaskTracker.Client/Pages/BoardDetails/BoardDetails.razor.cs
+++ TaskTracker.Client/Pages/BoardDetails/BoardDetails.razor.cs
@@ -29,38 +29,58 @@ namespace TaskTracker.Client.Pages.BoardDetails
         private MemberManager _memberManager = default!;
 
         public void OpenMembersDrawer() => _memberManager.OpenMembersDrawer();
+
         private void OnMembersDrawerClose() => _memberManager.OnMembersDrawerClose();
+
         private void OpenInviteModal() => _memberManager.OpenInviteModal();
+
         private void CloseInviteModal() => _memberManager.CloseInviteModal();
+
         private async Task HandleEditRole((Guid BoardRoleId, UserRole NewRole) roleUpdate) =>
             await _memberManager.HandleEditRoleAsync(roleUpdate);
+
         private async Task HandleRemoveMember(Guid boardRoleId) =>
             await _memberManager.HandleRemoveMemberAsync(boardRoleId);
+
         private async Task HandleUserSearch() => await _memberManager.HandleUserSearchAsync();
+
         private async Task HandleSendInvite() => await _memberManager.HandleSendInviteAsync();
+
         private void BackToSearch() => _memberManager.BackToSearch();
 
         private Guid GetCurrentUserId() => AuthStateService.CurrentUser?.Id ?? Guid.Empty;
 
         public List<MemberDto> Members => _memberManager.Members;
+
         public bool MembersDrawerVisible => _memberManager.MembersDrawerVisible;
+
         public bool InviteModalVisible => _memberManager.InviteModalVisible;
+
         public bool IsMembersLoading => _memberManager.IsMembersLoading;
+
         public string MembersErrorMessage => _memberManager.MembersErrorMessage;
+
         public InviteStep CurrentInviteStep => _memberManager.CurrentInviteStep;
+
+
         public string SearchEmail
         {
             get => _memberManager.SearchEmail;
             set => _memberManager.SearchEmail = value;
         }
+
         public string SearchError => _memberManager.SearchError;
+
         public UserDto? FoundUser => _memberManager.FoundUser;
+
         public UserRole SelectedRole
         {
             get => _memberManager.SelectedRole;
             set => _memberManager.SelectedRole = value;
         }
+
         public bool IsSearching => _memberManager.IsSearching;
+
         public bool IsSendingInvite => _memberManager.IsSendingInvite;
 
 
@@ -115,43 +135,62 @@ namespace TaskTracker.Client.Pages.BoardDetails
             await _memberManager.LoadMembersAsync();
         }
 
+
         private async Task SaveBoardTitle(string newTitle) =>
             await _boardManager.SaveBoardTitleAsync(boardId, newTitle);
+
 
         private async Task ArchiveBoard() =>
             await _boardManager.ArchiveBoardAsync(boardId);
 
+
         private Task OnTitleEditingChanged(bool isEditing) =>
             _boardManager.OnTitleEditingChanged(isEditing);
 
+
         private void ShowColumnModal() => _columnManager.ShowColumnModal();
+
         private Task HideColumnModal() => _columnManager.HideColumnModal();
+
         private async Task HandleColumnCreated(CreateColumnDto dto) =>
             await _columnManager.HandleColumnCreatedAsync(dto);
+
         private async Task OnColumnDelete(ColumnDto column) =>
             await _columnManager.OnColumnDeleteAsync(column);
+
         private async Task OnColumnTitleEditingChanged(Guid? columnId) =>
             await _columnManager.OnColumnTitleEditingChangedAsync(columnId);
+
         private async Task SaveColumnTitle((Guid ColumnId, string NewTitle) data) =>
             await _columnManager.SaveColumnTitleAsync(data);
+
         private async Task OnColumnEdit((Guid columnId, string newTitle) data) =>
             await _columnManager.OnColumnEditAsync(data);
 
+
         private async Task OnAddCard((string title, Guid columnId) data) =>
             await _cardManager.OnAddCardAsync(data);
+
         private async Task OnCardClick(CardDto card) =>
             await _cardManager.OnCardClickAsync(card);
+
         private Task HideCardDetailsModal() => _cardManager.HideCardDetailsModal();
+
         private Task OnCardTitleEditingChanged(bool isEditing) =>
             _cardManager.OnCardTitleEditingChanged(isEditing);
+
         private async Task SaveCardTitle(string newTitle) =>
             await _cardManager.SaveCardTitleAsync(newTitle);
+
         private async Task OnCardDelete(Guid cardId) =>
             await _cardManager.OnCardDeleteAsync(cardId);
+
         private async Task OnCommentSubmit(string commentContent) =>
             await _cardManager.OnCommentSubmitAsync(commentContent);
+
         private async Task OnCommentEdit((Guid commentId, string newContent) data) =>
             await _cardManager.OnCommentEditAsync(data);
+
         private async Task OnCommentDelete(Guid commentId) =>
             await _cardManager.OnCommentDeleteAsync(commentId);
     }
