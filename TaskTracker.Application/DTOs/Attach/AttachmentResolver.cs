a9c831be0805cff7cea732dc4a9121aec34a0e93
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-09-16 15:59:41 +0300

    added-models

---
+++ TaskTracker.Application/DTOs/Attach/AttachmentResolver.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using TaskTracker.Application.Features.Comment.Commands.Create;
+
+namespace TaskTracker.Application.DTOs.Attach;
+
+public class AttachmentResolver : IValueResolver<CreateCommentRequest, CreateCommentCommand, ICollection<AttachmentUpload>>
+{
+    public ICollection<AttachmentUpload> Resolve(CreateCommentRequest source, CreateCommentCommand destination,
+        ICollection<AttachmentUpload> destMember, ResolutionContext context)
+    {
+        var attachments = new List<AttachmentUpload>();
+
+        if (source.Files?.Any() == true)
+        {
+            foreach (var file in source.Files)
+            {
+                if (file.Length > 0)
+                {
+                    var stream = new MemoryStream();
+                    file.CopyTo(stream);
+                    stream.Position = 0;
+
+                    attachments.Add(new AttachmentUpload
+                    {
+                        Content = stream,
+                        FileName = file.FileName,
+                        ContentType = file.ContentType,
+                        Size = file.Length
+                    });
+                }
+            }
+        }
+
+        return attachments;
+    }
+}
---
+++ TaskTracker.Application/DTOs/AttachmentUpload.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTracker.Application.DTOs;
+public class AttachmentUpload
+{
+    public Stream Content { get; set; } = null!;
+    public string FileName { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
+    public long Size { get; set; }
+}
